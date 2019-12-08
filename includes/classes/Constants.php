54ebd8efd4c0182b301e9119859b73283e5c2ff0
Chanakya <chanakya.valluri@gmail.com>
2019-12-07 21:42:51 -0800

    login works

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -9,6 +9,8 @@ class Constants {
     public static $usernameCharacters = "Your username must be 5 and 25 characters";
     public static $firstNameCharacters = "Your last name must be 2 and 25 characters";
     public static $lastNameCharacters = "Your first name must be 2 and 25 characters";
+
+    public static $loginFailed = "Your username or password was incorrect";
 }
 
 ?>
--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -1,7 +1,15 @@
 <?php
 
 if (isset($_POST['loginButton'])) {
-    echo "login button was pressed";
+    // echo "login button was pressed";
+    $username = $_POST['loginUsername'];
+    $password = $_POST['loginPassword'];
+    
+    $result = $account->login($username, $password);
+
+    if ($result) {
+        header("Location: index.php");
+    }
 }
 
 ?>
