2e0500f4049a4ecf76be6280baac46eeaa797362
Chanakya <chanakya.valluri@gmail.com>
2019-12-07 20:01:48 -0800

    added constants

---
+++ includes/classes/Constants.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Constants {
+    public static $passwordsDoNotMatch = "Your passwords do not match";
+    public static $passwordNotAlphaNumeric = "Your passwords can only contain numbers and letters";
+    public static $passwordCharacters = "Your password must be 2 and 25 characters";
+    public static $emailInvalid = "Email is invalid";
+    public static $emailsDoNotMatch = "Your emails don't match";
+    public static $usernameCharacters = "Your username must be 5 and 25 characters";
+    public static $firstNameCharacters = "Your last name must be 2 and 25 characters";
+    public static $lastNameCharacters = "Your first name must be 2 and 25 characters";
+}
+
+?>
