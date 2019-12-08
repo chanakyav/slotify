45aed525afa20e5ee7abe8ac89bbd05296502c6a
Chanakya <chanakya.valluri@gmail.com>
2019-12-07 19:01:48 -0800

    code seperation

--- register.php
+++ register.php
@@ -1,11 +1,17 @@
+<?php
+    include("includes/handlers/register-handler.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to Slotify!</title>
 </head>
+
 <body>
     <div id="inputContainer">
         <form id="loginForm" action="register.php" method="POST">
@@ -13,21 +19,12 @@
 
             <p>
                 <label for="loginUsername">Username</label>
-                <input 
-                    id="loginUsername"
-                    name="loginUsername"
-                    type="text"
-                    placeholder="e.g. chanakyav"
-                    required>
+                <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. chanakyav" required>
             </p>
-            
+
             <p>
                 <label for="loginPassword">Password</label>
-                <input 
-                    id="loginPassword"
-                    name="loginPassword"
-                    type="password"
-                    required>
+                <input id="loginPassword" name="loginPassword" type="password" required>
             </p>
 
             <button type="submit" name="loginButton">Login</button>
@@ -39,72 +36,38 @@
 
             <p>
                 <label for="username">Username</label>
-                <input 
-                    id="username"
-                    name="username"
-                    type="text"
-                    placeholder="e.g. chanakyav"
-                    required>
+                <input id="username" name="username" type="text" placeholder="e.g. chanakyav" required>
             </p>
             <p>
                 <label for="firstName">First Name</label>
-                <input 
-                    id="firstName"
-                    name="firstName"
-                    type="text"
-                    placeholder="e.g. Chanakya"
-                    required>
+                <input id="firstName" name="firstName" type="text" placeholder="e.g. Chanakya" required>
             </p>
             <p>
                 <label for="lastName">Last Name</label>
-                <input 
-                    id="lastName"
-                    name="lastName"
-                    type="text"
-                    placeholder="e.g. Valluri"
-                    required>
+                <input id="lastName" name="lastName" type="text" placeholder="e.g. Valluri" required>
             </p>
             <p>
                 <label for="email">Email</label>
-                <input 
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="e.g. chanakyav@gmail.com"
-                    required>
+                <input id="email" name="email" type="email" placeholder="e.g. chanakyav@gmail.com" required>
             </p>
             <p>
                 <label for="email2">Confirm Email</label>
-                <input 
-                    id="email2"
-                    name="email2"
-                    type="email"
-                    placeholder="e.g. chanakyav@gmail.com"
-                    required>
+                <input id="email2" name="email2" type="email" placeholder="e.g. chanakyav@gmail.com" required>
             </p>
-            
+
             <p>
                 <label for="password">Password</label>
-                <input 
-                    id="password"
-                    name="password"
-                    type="password"
-                    placeholder="Your password"
-                    required>
+                <input id="password" name="password" type="password" placeholder="Your password" required>
             </p>
             <p>
                 <label for="password2">Confirm Password</label>
-                <input 
-                    id="password2"
-                    name="password2"
-                    type="password"
-                    placeholder="Your password"
-                    required>
+                <input id="password2" name="password2" type="password" placeholder="Your password" required>
             </p>
 
-            <button type="submit" name="RegisterButton">Sign Up</button>
+            <button type="submit" name="registerButton">Sign Up</button>
 
         </form>
     </div>
 </body>
+
 </html>
